2f063bfd822e318a8a3e4e29cb0964e15d4558e1
Joe Colley <josephthco@gmail.com>
2025-07-07 09:27:28 -0400

    wall fix

--- RockBucket/Assets/UI/Parts/ButtonJuice.cs
+++ RockBucket/Assets/UI/Parts/ButtonJuice.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonJuice : MonoBehaviour
 {
+    private Button button;
+    private bool canJuice = true;
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        canJuice = button.interactable;
+    }
+
     public void MouseEnter()
     {
+        if (!canJuice) return;
         transform.localScale = Vector3.one * 1.15f;
     }
 
     public void MouseExit()
     {
+        if (!canJuice) return;
         transform.localScale = Vector3.one;
     }
 }
